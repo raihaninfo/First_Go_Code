5baa29990889d2c0e5a4dce4608cb98a55980b63
Md Abu. Raihan <raihanmahmudi35@gmail.com>
2021-01-31 19:02:31 +0600

    variable and data type

--- main.go
+++ main.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Golang is best programing language")
+	// string type variable
+	var myName string = "My Name is Raihan"
+	// intiger type variable
+	var Age int = 21
+	// float type variable
+	var result float32 = 50.2
+	fmt.Println(myName, Age, result)
+	fmt.Println(result)
+	fmt.Println(Age)
 }
